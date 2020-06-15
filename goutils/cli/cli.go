417a3b75a857389accc87b724aef2d329857b6b9
zzara <36560280+zzara@users.noreply.github.com>
2020-06-14 21:37:17 -0700

    Update cli.go

--- goutils/cli/cli.go
+++ goutils/cli/cli.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+  "flag"
+)
+
+var flattenJson = flag.Bool("flattenjson", "false", "Flatten JSON to an un-nested JSON")
+var output = flag.Bool("out", "false", "Outputs the modified file")
+
 func main() {
+  if flattenJson != "false" {
+    flattenJson()
+  }
 }
